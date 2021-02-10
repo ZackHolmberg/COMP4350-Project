7cb926b0d665fb8726812c7b55a8314df4d4147a
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-02-10 00:58:03 -0600

    Changing the miner server to Development for socketio compatibility

--- backend/services/mining/src/__init__.py
+++ backend/services/mining/src/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(app,cors_allowed_origins='*')
+
 
 from src import mining
--- backend/services/mining/src/mining.py
+++ backend/services/mining/src/mining.py
@@ -1,6 +1,13 @@
-from src import app
+from src import app, socketio
 
 @app.route("/")
+@app.route("/mining")
 def index():
-    return "Hello From the Mining Service"
+    return "Hello From the Mining"
 
+@socketio.on('my event')
+def handle(message):
+    socketio.emit("my response", message, callback=msg)
+
+def msg(methods=['GET', 'POST']):
+    print('message was received!!!')
