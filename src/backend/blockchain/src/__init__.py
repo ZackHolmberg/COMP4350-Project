dd4fef3b3999a69083dc1e7cfef87d0aad6e1bd2
Zack Holmberg <zack_holmberg@outlook.com>
2021-02-16 16:09:41 -0600

    More edits

--- src/backend/blockchain/src/__init__.py
+++ src/backend/blockchain/src/__init__.py
@@ -1,4 +1,5 @@
-from src import routes
 from flask import Flask
 
 app = Flask(__name__)
+
+from src import routes
--- src/backend/services/transaction-api/src/__init__.py
+++ src/backend/services/transaction-api/src/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
-from src import routes
 
 app = Flask(__name__)
+
+from src import routes
