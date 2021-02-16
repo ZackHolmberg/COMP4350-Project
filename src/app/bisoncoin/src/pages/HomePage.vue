87446dbaab4b0141ec8245118de759eccf366a64
ZackHolmberg <zack_holmberg@outlook.com>
2021-02-15 18:37:32 -0600

    Issue 19: Initialize front-end app and create basic interface (#24)

---
+++ src/app/bisoncoin/src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="bison">BisonCoin</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomePage extends Vue {}
+</script>
+
+<style>
+.bison {
+  margin: 40px 0px;
+  color: gray;
+}
+</style>
---
+++ src/app/bisoncoin/src/pages/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <img
+      width="300px"
+      height="300px"
+      alt="BisonCoin logo"
+      src="../assets/BisonCoin.png"
+    />
+    <TextInput label="Username" />
+    <TextInput label="Password" />
+    <Button label="Login" />
+    <!-- TODO: Move this text and other hardcoded text to a separate file, as well as colours -->
+    <a href="TODO">Don't have an account? Create one now!</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TextInput from "../components/TextInput.vue";
+import Button from "../components/Button.vue";
+
+@Component({
+  components: {
+    TextInput,
+    Button,
+  },
+})
+export default class LoginPage extends Vue {}
+</script>
+
+<style>
+.home {
+  padding: 25px;
+}
+
+body {
+  background-color: lightgray;
+}
+
+img {
+  margin-bottom: 100px;
+  margin-top: 100px;
+}
+</style>
