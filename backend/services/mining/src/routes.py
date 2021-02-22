e8c8516f8721d7f4c3264ea53ed11a0b48176672
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-02-21 22:03:03 -0600

    Condition variable for Haulting Mining till one proof is found

--- backend/services/mining/src/routes.py
+++ backend/services/mining/src/routes.py
@@ -2,21 +2,30 @@ from src import app, socketio
 from flask import request, jsonify
 from .MiningPool import MiningPool
 import sys, os
+import threading
+
 sys.path.append(os.path.abspath(os.path.join('..', '')))
 
 from shared import HttpCode, FailureReturnString
 
+send = threading.Condition()
+ready_to_mine = False
 
 def sendToConnectedClients(transaction):
     # ToDo
     print("To Be Sent to Miner", transaction)
+
+    with send:
+        while not ready_to_mine:
+            send.wait()
+
+        socketio.emit("find_proof", transaction)
 
 transactions = MiningPool(sendToConnectedClients)
 
 @app.route("/")
 def index():
     return "Hello From the Mining"
-
 
 @app.route("/queue", methods= ["POST"])
 def addDataToQueue():
@@ -33,6 +42,13 @@ def addDataToQueue():
 @socketio.on('echo')
 def handle(message):
     socketio.emit("response", message, callback=msg)
+
+@socketio.on('proof')
+def handle(message):
+    with send:
+        ready_to_mine = True
+        send.notify_all()
+    # socketio.emit("response", message, callback=msg)
 
 def msg(methods=['GET', 'POST']):
     print('message was received!!!')
