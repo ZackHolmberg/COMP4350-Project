4fea7e2d1fde7c6099efce65dbe8d4296d16fe47
ZackHolmberg <zack_holmberg@outlook.com>
2021-02-23 17:00:38 -0600

    2: Implement core blockchain (#39)
    * Reorganized file structure
    
    * Added core blockchain functionality, relevant endpoints for this week's goal, and API documentation
    
    * Added some tests
    
    * Updated blockchain, endpoints, and tests
    
    * Added custom exception throwing
    
    * Merged from staging and fixed conflicts

---
+++ src/backend/blockchain/src/routes.py
@@ -0,0 +1,69 @@
+from src import app
+from .transaction import Transaction
+from .block import Block
+from .blockchain import blockchain
+from flask import request, jsonify
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join('..', '')))
+
+from shared import HttpCode
+
+@app.route("/")
+def index():
+    return "Hello Blockchain"
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain = []
+    for block in blockchain.chain:
+        chain.append(block.toJSON())
+    return jsonify(length=len(chain), chain=chain), HttpCode.OK
+
+
+@app.route('/proof', methods=['POST'])
+def proof():
+    data = request.json
+    # proof = data["proof"]
+    # newBlock = Block()
+    # TODO: Get other block data from the request data, including the
+    # transaction amount and to/from wallet IDs
+    # Then, add/subtract the transaction amount from the respective walletIds
+    # Using blockchain.addToWallet and blockchain.subtractFromWallet
+    return "proof was hit. You sent:"+request.json
+
+
+@app.route('/wallet/addWallet', methods=['POST'])
+def addWallet():
+    try:
+        data = request.json
+        walletId = data["walletId"]
+        success = blockchain.add_wallet(walletId)
+        return jsonify(success=success), HttpCode.CREATED
+    except Exception as e:
+        return jsonify(err=str(e)), HttpCode.BAD_REQUEST
+
+
+@app.route('/wallet/verifyAmount', methods=['POST'])
+def verifyAmount():
+    try:
+        data = request.json
+        walletId = data["walletId"]
+        amount = data["amount"]
+        valid = blockchain.verify_wallet_amount(walletId, amount)
+        return jsonify(valid=valid), HttpCode.OK
+    except Exception as e:
+        return jsonify(err=str(e)), HttpCode.BAD_REQUEST
+
+
+@app.route('/wallet/balance', methods=['GET'])
+def getWalletAmount():
+    try:
+        data = request.json
+        walletId = data["walletId"]
+        amount = blockchain.get_wallet_amount(walletId)
+        return jsonify(amount=amount), HttpCode.OK
+    except Exception as e:
+        return jsonify(err=str(e)), HttpCode.BAD_REQUEST
---
+++ src/backend/shared/HttpCodes.py
@@ -0,0 +1,17 @@
+class HttpCode:
+    OK: int = 200
+    CREATED: int = 201
+    NO_CONTENT: int = 204
+
+    MOVED_PERMANENTLY: int = 301
+    TEMPORARY_REDIRECT: int = 307
+
+    BAD_REQUEST: int = 400
+    UNAUTHORIZED: int = 401
+    FORBIDDEN: int = 403
+    NOT_FOUND: int = 404
+    METHOD_NOT_ALLOWED: int = 405
+
+    INTERNAL_SERVER_ERROR: int = 500
+    NOT_IMPLEMENTED: int = 501
+    BAD_GATEWAY: int = 502
