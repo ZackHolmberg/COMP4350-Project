48a0fc496530db1055a70d6cd9732f6d4330aa9e
Zack Holmberg <zack_holmberg@outlook.com>
2021-02-23 14:27:23 -0600

    Updated requirements to valid pytest version

--- src/backend/shared/HttpCodes.py
+++ src/backend/shared/HttpCodes.py
@@ -2,7 +2,7 @@ class HttpCode:
     OK: int = 200
     CREATED: int = 201
     NO_CONTENT: int = 204
-    
+
     MOVED_PERMANENTLY: int = 301
     TEMPORARY_REDIRECT: int = 307
 
@@ -11,7 +11,7 @@ class HttpCode:
     FORBIDDEN: int = 403
     NOT_FOUND: int = 404
     METHOD_NOT_ALLOWED: int = 405
-    
+
     INTERNAL_SERVER_ERROR: int = 500
     NOT_IMPLEMENTED: int = 501
     BAD_GATEWAY: int = 502
