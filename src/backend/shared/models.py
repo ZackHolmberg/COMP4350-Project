f941c978737fd5e2f5afb2fedf9a33e2017ca55e
Zack Holmberg <zack_holmberg@outlook.com>
2021-02-23 14:37:36 -0600

    Fix shared transaction mdoel

--- src/backend/shared/models.py
+++ src/backend/shared/models.py
@@ -2,10 +2,12 @@ import json
 
 
 class Transaction:
-    def __init__(self, to, from, amount):
-        self.toAddress = to
-        self.fromAddress = from
+
+    def __init__(self, to_address, from_address, amount):
+        self.to_address = to_address
+        self.from_address = from_address
         self.amount = amount
 
     def toJSON(self):
-        return json.dumps(self)
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True)
