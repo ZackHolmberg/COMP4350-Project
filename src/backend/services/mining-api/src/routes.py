2eb9e835406365000a70abfee68c6cc3dd039e66
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-02-23 17:38:02 -0600

    Mining Updates

--- src/backend/services/mining-api/src/routes.py
+++ src/backend/services/mining-api/src/routes.py
@@ -1,13 +1,49 @@
 from src import app, socketio
+from flask import request, jsonify
+from .MiningPool import MiningPool
+import sys, os
+import threading
+
+if os.environ.get('SERVICE_IN_DOCKER', False):
+    sys.path.append(os.path.abspath(os.path.join('..', '')))
+else:
+    sys.path.append(os.path.abspath(os.path.join('../..', '')))
+
+from shared import HttpCode, FailureReturnString
+
+def sendToConnectedClients(transaction):
+    # ToDo
+    print("To Be Sent to Miner", transaction)
+    socketio.emit("find_proof", transaction)
+
+transactions = MiningPool(sendToConnectedClients, True)
 
 @app.route("/")
-@app.route("/mining")
 def index():
     return "Hello From the Mining"
+
+@app.route("/queue", methods= ["POST"])
+def addDataToQueue():
+    data = request.get_json()
+
+    if data is None:
+        return jsonify(FailureReturnString.INCORRECT_PAYLOAD), HttpCode.BAD_REQUEST
+    
+    transactions.addToPool(data)
+
+    return jsonify(success=True), HttpCode.OK
+    
 
 @socketio.on('echo')
 def handle(message):
     socketio.emit("response", message, callback=msg)
+
+@socketio.on('proof')
+def handle_proofs(message):
+    # send the stuff off to blockchain
+
+    transactions.ready_to_mine()
+    # socketio.emit("response", message, callback=msg)
 
 def msg(methods=['GET', 'POST']):
     print('message was received!!!')
