922963a74b91d24b758e453731a2d205e93bcb47
Akshay Sharma <57690849+akshaysharma21@users.noreply.github.com>
2021-02-23 12:42:22 -0600

    48 initialize wallet service (#51)
    * refactored the services to keep the dockerfiles in a separate folder and enable passing the shared folder into the docker containers
    
    * Initialized the wallet service.
    
    * created a create and amount endpoint for the wallet service, made an endpoints document and wrote tests, begining the restructure with shared outside services
    
    * completed the merge with staging to implement new directory structure with dockerfiles outside services

---
+++ backend/services/wallet/src/routes.py
@@ -0,0 +1,61 @@
+from src import app
+from flask import request, jsonify
+import requests
+import sys, os
+import json
+
+if os.environ.get('SERVICE_IN_DOCKER',False):             
+    sys.path.append(os.path.abspath(os.path.join('..', '')))
+else:
+    sys.path.append(os.path.abspath(os.path.join('../..', '')))
+
+from shared import HttpCode, FailureReturnString
+
+@app.route("/")
+def index():
+    return "Hello from your wallet"
+
+
+@app.route("/create", methods = ['POST'])
+def createWallet():
+    data = request.get_json(force=True)
+
+    if (data is None) or ("public_key" not in data):
+        return jsonify(error = FailureReturnString.INCORRECT_PAYLOAD), HttpCode.BAD_REQUEST
+
+    req_body = {"walletId": data["public_key"]}
+
+    response = requests.post("http://localhost/blockchain/wallet/addWallet", req_body)
+
+    if response.status_code == HttpCode.OK:
+        return jsonify(
+            success = json.loads(response.text)["success"]
+            ), HttpCode.CREATED
+    else:
+        return jsonify(
+            error = json.loads(response.text)["error"]
+        ), HttpCode.BAD_REQUEST
+
+
+
+@app.route("/amount", methods = ['POST'])
+def getWalletAmount():
+    
+    data = request.get_json(force=True)
+
+    
+    if (data is None) or ("public_key" not in data):
+        return jsonify(error = FailureReturnString.INCORRECT_PAYLOAD), HttpCode.BAD_REQUEST
+
+    req_body = {"walletId": data["public_key"]}
+
+    response = requests.post("http://localhost/blockchain/wallet/balance", req_body)
+
+    if response.status_code == HttpCode.OK:   
+        return jsonify(
+            amount = json.loads(response.text)["amount"] 
+        ), HttpCode.OK
+    else:
+        return jsonify(
+            error = json.loads(response.text)["error"]
+        ), HttpCode.BAD_REQUEST
