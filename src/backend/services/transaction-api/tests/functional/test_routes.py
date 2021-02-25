4e902f95c2fb43910b5126f739ebeb1788fd90af
Abhi Sachdev <sachdevabhi74@gmail.com>
2021-02-24 23:35:41 -0600

    Transactions middleware refactor (#73)
    * Mining Updates
    
    * Proof handlers todo
    
    * Tests for Mining Pool
    
    * Merge conflicts
    
    * avoiding lint failure
    
    * Stub data
    
    * PR changes
    
    * Suggested changes
    
    * Forgot a little thing
    
    * Models and Connecting transactions, mining and blockchain
    
    * requirements and minor changes
    
    * bug fix
    
    * Fixing a test
    
    * Fixing all the tests
    
    * Solving the connection issues and bugs in the blockchain
    
    * Removing Signature Verification
    
    * Linting failure

--- src/backend/services/transaction-api/tests/functional/test_routes.py
+++ src/backend/services/transaction-api/tests/functional/test_routes.py
@@ -3,25 +3,13 @@ from src import app
 from uuid import uuid4
 import json
 import sys, os
-from ecdsa import SigningKey, VerifyingKey
-import base64
+
 from shared import HttpCode, FailureReturnString
 
 if os.environ.get('SERVICE_IN_DOCKER',False):             
     sys.path.append(os.path.abspath(os.path.join('..', '')))
 else:
     sys.path.append(os.path.abspath(os.path.join('../..', '')))
-
-
-@pytest.fixture(scope='module')
-def signing_key():
-    return SigningKey.generate()
-
-@pytest.fixture(scope='module')
-def public_key(signing_key):
-    pubk = signing_key.verifying_key.to_string()
-    pubk = base64.b64encode(pubk)
-    return pubk
 
 @pytest.fixture(scope='module')
 def test_client():
@@ -33,6 +21,7 @@ def test_client():
     yield test_client
 
     ctx.pop()
+
 
 @pytest.fixture(scope='module')
 def json_header():
@@ -57,10 +46,8 @@ def test_home_page(test_client):
 
 def test_create_transaction_incorrect_payload(test_client, json_header):
     data = {
-        'id': '21435',
         'from' : 'user1',
         'to' : 'user2',
-        'amount' : 99
     }
     url = '/create'
 
@@ -70,58 +57,55 @@ def test_create_transaction_incorrect_payload(test_client, json_header):
     assert b"err" in response.data
     assert FailureReturnString.INCORRECT_PAYLOAD.value.encode() in response.data
 
-def test_create_transaction_wrong_key(test_client, json_header):
+def test_create_transaction_wrong_wallet_amount(test_client, json_header, requests_mock):
     data = {
-        'id': '21435',
         'from' : 'user1',
         'to' : 'user2',
         'amount' : 99,
-        'signature' : 'A wrong signature'
     }
     url = '/create'
-
-    response = test_client.post(url, data=json.dumps(data), headers=json_header)
-
-    assert response.status_code == HttpCode.BAD_REQUEST.value
-    assert b"err" in response.data
-
-def test_create_transaction_correct_key_wrong_signature(test_client, json_header, public_key):
-    
-    data = {
-        'id': '21435',
-        'from' : public_key.decode(),
-        'to' : 'user2',
-        'amount' : 99,
-        'signature' : 'A wrong signature'
-    }
-    url = '/create'
-
-    response = test_client.post(url, data=json.dumps(data), headers=json_header)
-
-    assert response.status_code == HttpCode.BAD_REQUEST.value
-    assert b"err" in response.data
-
-def test_create_transaction_correct_payload(test_client, json_header, signing_key, public_key, requests_mock):
-    ## sign a transaction
-    to_sign = str(uuid4())
-    signature = signing_key.sign(to_sign.encode())
-
-    signature = base64.b64encode(signature).decode()
 
     requests_mock.post("http://blockchain:5000/wallet/verifyAmount",
-                       json={"valid": True}, status_code=200)
+                       json={"valid": False}, status_code=400)
 
+    response = test_client.post(url, data=json.dumps(data), headers=json_header)
+
+    assert response.status_code == HttpCode.BAD_REQUEST.value
+    assert b"false" in response.data
+
+def test_create_transaction_mining_fail(test_client, json_header, requests_mock):
+    
     data = {
-        'id' : to_sign,
-        'from' : public_key.decode(),
+        'from' : "user1",
         'to' : 'user2',
         'amount' : 99,
-        'signature' : signature
     }
     url = '/create'
 
     requests_mock.post("http://mining:5000/queue",
-                       json={"success": True}, status_code=200)
+                       json={"err": "something went wrong"}, status_code=500)
+
+
+    response = test_client.post(url, data=json.dumps(data), headers=json_header)
+
+    assert response.status_code == HttpCode.INTERNAL_SERVER_ERROR.value
+    assert b"err" in response.data
+
+def test_create_transaction_correct_payload(test_client, json_header, requests_mock):
+
+    requests_mock.post("http://blockchain:5000/wallet/verifyAmount",
+                       json={"valid": True}, status_code=200)
+
+    requests_mock.post("http://mining:5000/queue",
+                       json={"success": True}, status_code=201)
+    data = {
+        'from' : "user1",
+        'to' : 'user2',
+        'amount' : 99,
+    }
+
+    url = '/create'
+
 
     response = test_client.post(url, data=json.dumps(data), headers=json_header)
 
--- src/backend/shared/models.py
+++ src/backend/shared/models.py
@@ -1,25 +1,12 @@
 import json
 
 
-class BlockchainTransaction:
+class Transaction:
 
     def __init__(self, from_address, to_address, amount):
         self.from_address = from_address
         self.to_address = to_address
         self.amount = amount
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True)
-
-class ClientTransaction:
-
-    def __init__(self, id, from_address, to_address, amount, signature):
-        self.id = id
-        self.from_address = from_address
-        self.to_address = to_address
-        self.amount = amount
-        self.signature = signature
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
