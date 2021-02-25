bb54a20647d4cc3b8c40af65a4d7f7ebddc6d636
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-02-25 16:46:39 -0600

    Fixing Sprint 2 Bug

--- src/backend/shared/ReturnStrings.py
+++ src/backend/shared/ReturnStrings.py
@@ -4,4 +4,5 @@ from enum import Enum
 class FailureReturnString(Enum):
     INCORRECT_PAYLOAD = "Please send correct json payload"
     TRANSACTION_VERFICATION_FAILURE = "Unable to Verify the Transaction"
+    WALLET_VERFICATION_FAILURE = "Unable to Verify the Wallet Amount"
     UNKNOWN_FAILURE = "Something went wrong, please try again"
