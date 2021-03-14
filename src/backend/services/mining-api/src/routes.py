804cce5d794ffd7fc4ef1d38bfe6e67ac7feca35
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-03-14 03:22:44 -0500

    remove debug statements

--- src/backend/services/mining-api/src/routes.py
+++ src/backend/services/mining-api/src/routes.py
@@ -56,11 +56,6 @@ transactions = MiningPool(send_to_connected_clients, True)
 def index():
     return "Hello From the Mining"
 
-@app.route("/empty")
-def empty_test():
-    transactions.ready_to_mine()
-    return jsonify(success=True), 200
-
 @app.route("/queue", methods=["POST"])
 def add_data_to_queue():
     data = request.get_json()
@@ -87,13 +82,13 @@ def valid_proof(hash_, nonce) -> bool:
 def client_connect():
     global connected_clients
     connected_clients += 1
-    print("CLIENT CONNECTED", connected_clients)
+    print("MINING CLIENT CONNECTED", connected_clients)
 
 @socketio.on('disconnect')
 def client_disconnect():
     global connected_clients
     connected_clients -= 1
-    print("CLIENT DISCONNECTED", connected_clients)
+    print("MINING CLIENT DISCONNECTED", connected_clients)
 
 
 @socketio.on('echo')
@@ -104,36 +99,26 @@ def handle(message):
 @socketio.on('proof')
 def handle_proofs(message):
     global ongoing_proof, ongoing_transaction, blockchain_url, transactions
-    print("ZACK OUTPUT - GETTING HERE 0", file=sys.stderr)
-    if (ongoing_proof == message['id']):
-        print("ZACK OUTPUT - GETTING HERE 0 but before 1", file=sys.stderr)
 
+    if (ongoing_proof == message['id']):
         if valid_proof(message["proof"], message["nonce"]):
-            print("ZACK OUTPUT - GETTING HERE 1", file=sys.stderr)
             # ignore the rest of the clients that try to send the proof
             ongoing_proof = None
-
             socketio.emit("stopProof", None)
-            print("ZACK OUTPUT - GETTING HERE 2", file=sys.stderr)
             # new transactions are now ready to be mined
             block_data = {}
             # send transactions to blockchain
             for key, value in message.items():
                 block_data[key] = value
-            print("ZACK OUTPUT - GETTING HERE 3", file=sys.stderr)
+
             for key, value in ongoing_transaction.items():
                 block_data[key] = value
-            print("ZACK OUTPUT - GETTING HERE 4", file=sys.stderr)
 
             ongoing_transaction = None
             transactions.ready_to_mine()
-            print("ZACK OUTPUT - GETTING HERE 5", file=sys.stderr)
 
             send_post_request(blockchain_url.format("addBlock"), block_data)
-            print("ZACK OUTPUT - GETTING HERE 6", file=sys.stderr)
-
             emit('reward', None)
-            print("ZACK OUTPUT - GETTING HERE 7", file=sys.stderr)
             return
 
 
