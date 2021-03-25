b48afd1c83b7aa4aa663e14c28bd9f275c7f64af
Abhishek Sachdev <sachdevabhi74@gmail.com>
2021-03-25 02:43:11 -0500

    Refactor for readability

--- src/backend/run_tests.py
+++ src/backend/run_tests.py
@@ -6,6 +6,11 @@ directories = ["./blockchain",
                 "./services/users-api", 
                 "./services/mining-api"]
 
+change_directory = ["cd"]
+run_coverage_pytest = ["python", "-m", "coverage", "run", "--source=./src", "-m", "pytest"]
+get_coverage_report = ["python", "-m", "coverage", "report"]
+_and = ["&&"]
+
 for d in directories:
-    p = Popen(["cd", d, "&&", "python", "-m", "coverage", "run", "--source=./src", "-m", "pytest", "&&", "python", "-m", "coverage", "report"], shell= True)
+    p = Popen( change_directory + [d] + _and + run_coverage_pytest + _and + get_coverage_report, shell= True)
     p.communicate()
